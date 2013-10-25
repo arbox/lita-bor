1d0a7223d2f0d81f8240cdf2b2d0bdbe1c293800
Andrei Beliankou <arbox@yandex.ru>
2013-10-26 00:42:28 +0200

    Getting a simple quote.

--- lib/lita/handlers/bor.rb
+++ lib/lita/handlers/bor.rb
@@ -1,8 +1,14 @@
-require "lita"
+require 'lita'
+require 'net/http'
 
 module Lita
   module Handlers
     class Bor < Handler
+      route(/^bor$/, :bor)
+    end
+
+    def bor
+      Net::HTTP.get(URI('http://bash.im/forweb/?u'))
     end
 
     Lita.register_handler(Bor)
